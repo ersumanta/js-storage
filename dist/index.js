1a9bdcf41ee229e91212dcdcaaf2599f8e16c3bf
Sumanta Sarkar <sumanta@nextgn.co.uk>
2017-04-15 20:01:23 +0100

    Updated Babel Config

--- dist/index.js
+++ dist/index.js
@@ -1,28 +1,76 @@
-import * as _ from "underscore";
+"use strict";
 
-class JsStorage {
-  static initialize() {
-    console.log("--- js storage initialise ---");
-    if (!this.obj) {
-      this.obj = {};
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () {
+  function defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);
     }
-  }
+  }return function (Constructor, protoProps, staticProps) {
+    if (protoProps) defineProperties(Constructor.prototype, protoProps);if (staticProps) defineProperties(Constructor, staticProps);return Constructor;
+  };
+}();
 
-  static get(id) {
-    return this.obj[id];
-  }
+var _underscore = require("underscore");
 
-  static getAndClear(id) {
-    let oldVal = _.clone(this.obj[id]);
-    delete this.obj[id];
-    return oldVal;
-  }
+var _ = _interopRequireWildcard(_underscore);
 
-  static set(id, value) {
-    this.obj[id] = value;
+function _interopRequireWildcard(obj) {
+  if (obj && obj.__esModule) {
+    return obj;
+  } else {
+    var newObj = {};if (obj != null) {
+      for (var key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key];
+      }
+    }newObj.default = obj;return newObj;
   }
 }
 
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+}
+
+var JsStorage = function () {
+  function JsStorage() {
+    _classCallCheck(this, JsStorage);
+  }
+
+  _createClass(JsStorage, null, [{
+    key: "initialize",
+    value: function initialize() {
+      console.log("--- js storage initialise ---");
+      if (!this.obj) {
+        this.obj = {};
+      }
+    }
+  }, {
+    key: "get",
+    value: function get(id) {
+      return this.obj[id];
+    }
+  }, {
+    key: "getAndClear",
+    value: function getAndClear(id) {
+      var oldVal = _.clone(this.obj[id]);
+      delete this.obj[id];
+      return oldVal;
+    }
+  }, {
+    key: "set",
+    value: function set(id, value) {
+      this.obj[id] = value;
+    }
+  }]);
+
+  return JsStorage;
+}();
+
 JsStorage.initialize();
 
-export default JsStorage;
+exports.default = JsStorage;
